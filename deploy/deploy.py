402e793de0cb31bcd261b21e95bb12e2474dfb92
nhatphong5954 <pukinanasi27@gmail.com>
2025-05-05 23:17:57 +0700

    Hoàn thiện mint_NFT

--- deploy/deploy.py
+++ deploy/deploy.py
@@ -9,7 +9,7 @@ print("Connected:", w3.isConnected())
 account = w3.eth.account.from_key(PRIVATE_KEY)
 
 # Load ABI từ file (export từ Remix)
-with open("contracts/MyNFT_abi.json", "r") as f:
+with open("contracts/MyNFT_contract.json", "r") as f:
     contract_data = json.load(f)
 
 # Deploy contract
@@ -24,3 +24,6 @@ tx = contract.constructor().buildTransaction({
 signed_tx = w3.eth.account.sign_transaction(tx, PRIVATE_KEY)
 tx_hash = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
 print(f"Contract deployed! TX Hash: {tx_hash.hex()}")
+
+contract_address = w3.eth.get_transaction_receipt(tx_hash)["contractAddress"]
+print(f"Deployed at: {contract_address}")
--- scripts/mint_NFT.py
+++ scripts/mint_NFT.py
@@ -1,23 +1,23 @@
 from web3 import Web3
 import json
-from config import PRIVATE_KEY, GANACHE_URL, CONTRACT_ADDRESS, RECIPIENT_ADDRESS
+from config.config import PRIVATE_KEY, GANACHE_URL, CONTRACT_ADDRESS, RECIPIENT_ADDRESS
 
 w3 = Web3(Web3.HTTPProvider(GANACHE_URL))
+print("Connected:", w3.is_connected())
+
+with open("contracts/MyNFT_contract.json", "r") as f:
+    contract_data = json.load(f)
+
+nft_contract = w3.eth.contract(address=CONTRACT_ADDRESS, abi=contract_data["abi"])
 
 
-with open("contracts/MyNFT_abi.json", "r") as f:
-    abi = json.load(f)
-
-nft_contract = w3.eth.contract(address=CONTRACT_ADDRESS, abi=abi)
-
-
-tx = nft_contract.functions.mintNFT(RECIPIENT_ADDRESS).buildTransaction({
+tx = nft_contract.functions.mintNFT(RECIPIENT_ADDRESS, "https://example.com/metadata.json").build_transaction({
     "from": RECIPIENT_ADDRESS,
-    "nonce": w3.eth.getTransactionCount(RECIPIENT_ADDRESS),
+    "nonce": w3.eth.get_transaction_count(RECIPIENT_ADDRESS),
     "gas": 200000,
-    "gasPrice": w3.toWei("50", "gwei")
+    "gasPrice": w3.to_wei("50", "gwei")
 })
 
 signed_tx = w3.eth.account.sign_transaction(tx, PRIVATE_KEY)
-tx_hash = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
+tx_hash = w3.eth.send_raw_transaction(signed_tx.raw_transaction)
 print(f"NFT minted! TX Hash: {tx_hash.hex()}")
